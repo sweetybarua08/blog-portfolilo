bdaee28515ee9a38b6a7e0c26e19f0181496cb4b
Sweety Barua <you@example.com baruahsweety08@gmail.com>
2025-08-23 22:32:28 +0600

    pushing 1st

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -58,7 +58,7 @@ export const AboutSection = () => {
                 </div>
               </div>
             </div>
-            <div className="gradient-border p-6 card-hover">
+            {/* <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
                   <User className="h-6 w-6 text-primary" />
@@ -71,7 +71,7 @@ export const AboutSection = () => {
                   </p>
                 </div>
               </div>
-            </div>
+            </div> */}
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
